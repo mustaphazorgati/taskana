74c9a32921df0e6efebe6e1b69612f1906b1dfec
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2021-02-12 11:48:42 +0100

    modified DishDecider to use our custom dmn table

--- dmn-engine-java-main-method/src/main/java/org/camunda/bpm/example/DishDecider.java
+++ dmn-engine-java-main-method/src/main/java/org/camunda/bpm/example/DishDecider.java
@@ -28,47 +28,31 @@ import org.camunda.bpm.engine.variable.Variables;
 
 public class DishDecider {
 
-  public static void printUsage(String errorMessage, int exitCode) {
-    System.err.println("Error: " + errorMessage);
-    System.err.println("Usage: java -jar DishDecider.jar SEASON GUEST_COUNT\n\n\tSEASON: the current season (Spring, Summer, Fall or Winter)\n\tGUEST_COUNT: number of guest to expect");
-    System.exit(exitCode);
-  }
-
   public static void main(String[] args) {
-    // parse arguments
-    if (args.length != 2) {
-      printUsage("Please specify the current season and the expected number of guests.", 1);
-    }
-    String season = args[0];
-
-    Integer guestCount = 0;
-    try  {
-      guestCount = Integer.parseInt(args[1]);
-    }
-    catch (NumberFormatException e) {
-      printUsage("The number of expected guests must be a number", 2);
-    }
+    String classificationName = "Widerspruch";
 
     // prepare variables for decision evaluation
-    VariableMap variables = Variables
-      .putValue("season", season)
-      .putValue("guestCount", guestCount);
+    VariableMap variables =
+        Variables.putValue("classificationName", classificationName);
 
     // create a new default DMN engine
     DmnEngine dmnEngine = DmnEngineConfiguration.createDefaultDmnEngineConfiguration().buildEngine();
 
     // parse decision from resource input stream
-    InputStream inputStream = DishDecider.class.getResourceAsStream("dish-decision.dmn11.xml");
+    InputStream inputStream = DishDecider.class.getResourceAsStream("dmn-table.dmn");
 
     try {
-      DmnDecision decision = dmnEngine.parseDecision("decision", inputStream);
+      DmnDecision decision = dmnEngine.parseDecision("example", inputStream);
 
       // evaluate decision
       DmnDecisionTableResult result = dmnEngine.evaluateDecisionTable(decision, variables);
 
       // print result
-      String desiredDish = result.getSingleResult().getSingleEntry();
-      System.out.println("Dish Decision:\n\tI would recommend to serve: " + desiredDish);
+      // TODO: prevent NPE
+      String workbasketKey = result.getSingleResult().getEntry("workbasketKey");
+      String domain = result.getSingleResult().getEntry("domain");
+      System.out.println("WorkbkasketKey: " + workbasketKey);
+      System.out.println("domain: " + domain);
 
     }
     finally {
