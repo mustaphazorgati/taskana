d261edb84227ac1b6f47c2d3cdd3d3b5d9d40349
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2021-02-12 11:50:44 +0100

    used try-with-resource-statement for dmn table

--- dmn-engine-java-main-method/src/main/java/org/camunda/bpm/example/DishDecider.java
+++ dmn-engine-java-main-method/src/main/java/org/camunda/bpm/example/DishDecider.java
@@ -1,24 +1,7 @@
-/*
- * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
- * under one or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information regarding copyright
- * ownership. Camunda licenses this file to you under the Apache License,
- * Version 2.0; you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.camunda.bpm.example;
 
 import java.io.IOException;
 import java.io.InputStream;
-
 import org.camunda.bpm.dmn.engine.DmnDecision;
 import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
 import org.camunda.bpm.dmn.engine.DmnEngine;
@@ -28,7 +11,7 @@ import org.camunda.bpm.engine.variable.Variables;
 
 public class DishDecider {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     String classificationName = "Widerspruch";
 
     // prepare variables for decision evaluation
@@ -38,10 +21,8 @@ public class DishDecider {
     // create a new default DMN engine
     DmnEngine dmnEngine = DmnEngineConfiguration.createDefaultDmnEngineConfiguration().buildEngine();
 
-    // parse decision from resource input stream
-    InputStream inputStream = DishDecider.class.getResourceAsStream("dmn-table.dmn");
-
-    try {
+    try (InputStream inputStream = DishDecider.class.getResourceAsStream("dmn-table.dmn")) {
+      // parse decision from resource input stream
       DmnDecision decision = dmnEngine.parseDecision("example", inputStream);
 
       // evaluate decision
@@ -53,17 +34,7 @@ public class DishDecider {
       String domain = result.getSingleResult().getEntry("domain");
       System.out.println("WorkbkasketKey: " + workbasketKey);
       System.out.println("domain: " + domain);
-
-    }
-    finally {
-      try {
-        inputStream.close();
-      }
-      catch (IOException e) {
-        System.err.println("Could not close stream: "+e.getMessage());
-      }
     }
   }
-
-
+  
 }
