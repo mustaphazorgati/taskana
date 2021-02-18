b7b0a76a84c4ac0bd58b10ab450494c90af9d2e2
Patrick Treyer <ptreyer@users.noreply.github.com>
2021-02-18 12:14:56 +0100

    TSK-1514: Updated the cypress tests to enable local execution

--- web/cypress/integration/login/login.spec.js
+++ web/cypress/integration/login/login.spec.js
@@ -1,21 +1,7 @@
 context('TASKANA Login', () => {
   
-  if (Cypress.env('isLocal')) {
-    it('should not be run because its local development', () => {
-      cy.log('Local development - No need for testing login functionality');
-      expect(true).to.be.true;
-    });
-  } else {
-    it('should redirect to login when not logged in yet', () => {
-      cy.visit(Cypress.env('baseUrl') + '/taskana/workplace');
-
-      cy.location().should((location) => {
-        expect(location.href).to.eq(Cypress.env('baseUrl') + '/login');
-      });
-    });
-
-    it('should login via taskana login page', () => {
+    it('should login depending on current configuration', () => {
       cy.loginAs('admin');
     });
-  }
+  
 });
--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -52,20 +52,22 @@ Cypress.Commands.add('reloadPageWithWait', () => {
 });
 
 Cypress.Commands.add('loginAs', (username) => {
+  if (Cypress.env('isLocal')) {
+      cy.log('Local development - No need for testing login functionality');
+  } else {
+      cy.visit(Cypress.env('baseUrl') + '/login');
 
-  cy.visit(Cypress.env('baseUrl') + '/login');
-
-  cy.get('#username').type('admin').should('have.value', 'admin');
-
-  cy.get('#password').type('admin').should('have.value', 'admin');
-
-  cy.get('#login-submit').click();
-
-  cy.reload();
-  cy.wait(Cypress.env('pageReload'));
-
-  cy.location().should((location) => {
-    expect(location.href).to.eq(Cypress.env('baseUrl') + '/#/taskana/workplace/tasks');
-  });
-  
+      cy.get('#username').type('admin').should('have.value', 'admin');
+    
+      cy.get('#password').type('admin').should('have.value', 'admin');
+    
+      cy.get('#login-submit').click();
+    
+      cy.reload();
+      cy.wait(Cypress.env('pageReload'));
+    
+      cy.location().should((location) => {
+        expect(location.href).to.eq(Cypress.env('baseUrl') + '/#/taskana/workplace/tasks');
+      });
+    }
 });
